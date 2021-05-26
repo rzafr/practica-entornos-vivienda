b35a5cb3ebeea2cc6075c61c1256662f92856020
rzafr <rubenzafraramirez@gmail.com>
2021-05-26 16:36:53 +0200

    Listo

--- src/Hogar/Hogar.java
+++ src/Hogar/Hogar.java
@@ -25,16 +25,5 @@ public class Hogar {
                 
     }
     
-    public void imprimirVivienda() {
-    	
-    	Vivienda casa1 = new Vivienda(120000);
-    	
-    	System.out.println("Precio: " + casa1.getPrecio());
-        System.out.println("Número de habitaciones: "+ casa1.getNumHabitaciones());
-        System.out.println("Superficie en m2: " + casa1.getSuperficie());
-        System.out.println("¿Dispone de parking?: " + casa1.isParking());
-        System.out.println("Estado: "+ casa1.getEstado());
-        System.out.println("Propietario: "+ casa1.getPropietario());
-        
-    }
+    
 }
